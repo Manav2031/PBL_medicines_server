325426e0a1423b84d7e9f21e7eb61395807f41a8
varadpundlik <varadpundlik@gmail.com>
2023-03-19 14:48:30 +0530

    shifted server to another port

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ const medRouter = require('./routes/medRoutes')
 const prescRouter=require('./routes/precRoutes')
 
 const app = express()
-const port = 3000
+const port = 5000
 
-app.use(
-    express.urlencoded({ extended: true })
-);
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
 app.use(bodyParser.json())
@@ -24,4 +21,5 @@ app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 mongoose.connect("mongodb://0.0.0.0:27017/medicalDB", { useNewUrlParser: true, useUnifiedTopology: true }) //on upgrading node version to 18.x.x use 0.0.0.0 instead of localhost
     .catch(err => { console.log(err) })
     .then(console.log("DB connected"));
+
 app.listen(port, () => console.log(`app listening on port ${port}!`))

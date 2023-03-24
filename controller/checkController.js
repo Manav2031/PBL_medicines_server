ab2739e4d9542c5f8cb8142c805f07ea81c03361
varadpundlik <varadpundlik@gmail.com>
2023-03-24 15:07:43 +0530

    check availability of medicine s version 1

--- controller/checkController.js
+++ controller/checkController.js
@@ -1,22 +1,33 @@
 const medicines=require('../model/medicalSchema')
 
-exports.checkAvailability=(req,res)=>{
+
+exports.checkAvailability=async (req,res)=>{
     try{
-        var checked=1;
+        var checked=true;
         const presc={                                 //add prescription to database
             name:req.body.name,
             doctor:req.body.doctor,
             email:req.body.email,
             med:req.body.med
         };
-        presc.med.map(async (item)=>{                                  //subtract quantity of ordered medicines from stock
+        // presc.med.forEach(async (item)=>{                                  //subtract quantity of ordered medicines from stock
+        //     const filter={name:item.name};
+        //     let medItem=await medicines.findOne(filter);
+        //     if(medItem.quantity<=item.quantity){
+        //         checked=false;
+        //     }
+        // })
+        for(i=0;i<presc.med.length;i++){
+            const item=presc.med[i];
             const filter={name:item.name};
             let medItem=await medicines.findOne(filter);
-            if(medItem.quantity>=item.quantity){
-                checked=0
+            if(medItem.quantity<=item.quantity){
+                checked=false;
+                break;
             }
-        })
-        res.status(200).json({
+        }
+        console.log(checked)
+        return res.status(200).json({
             availability:checked
         });
 

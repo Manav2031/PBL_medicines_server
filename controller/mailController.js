8a165173ec06085baa7a8e93c8398bc073cc0a58
Varad Pundlik <93179552+varadpundlik@users.noreply.github.com>
2023-04-18 10:17:12 +0000

    added mailgun api

--- controller/mailController.js
+++ controller/mailController.js
@@ -1,3 +1,20 @@
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+
 exports.sendMail=(req,res)=>{
+    const mg = mailgun.client({
+        username: 'api',
+        key: '',
+    });
+    mg.messages
+        .create(sandbox54e0d3ba5c334b85973ef01c1aaa0fce.mailgun.org, {
+            from: "Mailgun Sandbox <postmaster@sandbox54e0d3ba5c334b85973ef01c1aaa0fce.mailgun.org>",
+            to: [res.body.email],
+            subject: "Hello",
+            text: "Testing some Mailgun awesomness!",
+        })
+        .then(msg => console.log(msg)) // logs response data
+        .catch(err => console.log(err));
     
 }

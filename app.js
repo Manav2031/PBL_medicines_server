d5bff1b89a80d5bc495dc26c336033ed272590c3
varadpundlik <varadpundlik@gmail.com>
2023-05-23 17:50:58 +0530

    build version

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const path=require('path')
 
 const medRouter = require('./routes/medRoutes')
 const prescRouter=require('./routes/precRoutes')
@@ -21,7 +22,11 @@ app.use('/med', medRouter);
 app.use('/presc',prescRouter);
 app.use('/check',checkRouter);
 app.use('/mail',mailRouter)
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
 
 mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true }) //on upgrading node version to 18.x.x use 0.0.0.0 instead of localhost
     .catch(err => { console.log(err) })
